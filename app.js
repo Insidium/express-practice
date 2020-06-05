45d5fe813ccb33da6f38871d2d8db727ef501be1
Insidium <ryanchallen@outlook.com>
2020-06-05 10:40:52 +1000

    Set up database connection with mongoose, with error handling

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const dbConnect = 'mongodb://localhost/animal_shelter_app';
+
+mongoose.connect(
+	dbConnect,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		userFindAndModify: false,
+	},
+	(err) => {
+		if (err) {
+			console.log('Error connecting database', err);
+		} else {
+			console.log('Connected to database!');
+		}
+	}
+);
+
 app.listen(port, () =>
 	console.log(`Animal Shelter server is listening on port ${port}`)
 );
