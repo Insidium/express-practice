c028a19b2f1c1ddb29ce5805203365d0fc853628
Insidium <ryanchallen@outlook.com>
2020-06-05 10:30:40 +1000

    Added port details, used cors and body-parser and set listening with message

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+const port = process.env.port || 3008;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () =>
+	console.log(`Animal Shelter server is listening on port ${port}`)
+);
